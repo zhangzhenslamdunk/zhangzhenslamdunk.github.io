5ab54dcc5520e1e0f2767d3d0eb452aa47e554e9
Zhang Zhen <zhangzhenslamdunk@hotmail.com>
2018-05-02 16:55:04 +0800

    Update this2.js

--- LocalChatbot/this2.js
+++ LocalChatbot/this2.js
@@ -80,7 +80,7 @@ function newEntry() {
      }
      */
     
-    messages.push("debug1");
+    messages.push("debug2");
     // from heroku
     
     //Speech(lastUserMessage);  //says what the user typed outloud
